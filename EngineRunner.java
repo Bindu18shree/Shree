1a10d0d481eae33bbdec2bd9eb5341fdbb603c3d
unknown <bindushreep.xworkz@gmail.com>
2024-08-23 12:38:00 +0530

    22/08/24

---
+++ EngineRunner.java
@@ -0,0 +1,7 @@
+class EngineRunner{
+	
+	public static void main(String[] args){
+		
+		Engine.carEngine("Kryotech turbocharged diesel engine" , 2.0f);
+	}
+}
---
+++ PositionRunner.java
@@ -0,0 +1,7 @@
+class PositionRunner{
+	
+	public static void main(String [] args){
+		
+		Position.post("Data Scientist" , 1501180);
+	}
+}
