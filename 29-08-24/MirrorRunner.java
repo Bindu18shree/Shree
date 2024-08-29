35153660b22a2b1e66ffd02cf9ab75508f88b972
unknown <bindushreep.xworkz@gmail.com>
2024-08-30 00:34:45 +0530

    29/08/24

---
+++ 29-08-24/MirrorRunner.java
@@ -0,0 +1,21 @@
+class MirrorRunner{
+	
+	public static void main(String [] args){
+		
+		Mirror.type = "Printed Long Wall Mirror ";
+		
+		Mirror.code = "466298548_multi";
+		
+		Mirror.weight = 12000;
+		
+		Mirror.height = 121.926d;
+		
+		Mirror.length = 45.72f;
+		
+		Mirror.price = 3240;
+		
+		
+		
+		Mirror.display();
+	}
+}
