5b17983be967db188654c499aadade6ce42a1b63
unknown <bindushreep.xworkz@gmail.com>
2024-09-10 14:16:33 +0530

    Wrapper classes 2

---
+++ wrapper classes 2/Toys.java
@@ -0,0 +1,8 @@
+class Toys{
+	
+	String price = "300";
+	
+	Integer cost = Integer.parseInt("250");
+	
+
+}
---
+++ wrapper classes 2/ToysRunner.java
@@ -0,0 +1,10 @@
+class ToysRunner{
+	
+	public static void main(String [] args){
+		
+		Toys toys = new Toys();
+		
+		System.out.println("Price of the toy ==" +toys.cost);
+		
+	}
+}
