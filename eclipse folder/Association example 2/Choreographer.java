dc2d79beaaf90cdf668577be33db36568ff91d7c
unknown <bindushreep.xworkz@gmail.com>
2024-09-14 20:27:46 +0530

    Association example 2

---
+++ eclipse folder/Association example 2/Choreographer.java
@@ -0,0 +1,37 @@
+package com.xworkz.studio.boot;
+
+public class Choreographer {
+	
+	public String nameOfTheChoreographer = "Bhuvan";
+	
+	public int noOfBatchesTaken = 3;
+	
+	public long contactNumberOfTheChoregrapher = 823432562l;
+
+	public String getNameOfTheChoreographer() {
+		return nameOfTheChoreographer;
+	}
+
+	public void setNameOfTheChoreographer(String nameOfTheChoreographer) {
+		this.nameOfTheChoreographer = nameOfTheChoreographer;
+	}
+
+	public int getNoOfBatchesTaken() {
+		return noOfBatchesTaken;
+	}
+
+	public void setNoOfBatchesTaken(int noOfBatchesTaken) {
+		this.noOfBatchesTaken = noOfBatchesTaken;
+	}
+
+	public long getContactNumberOfTheChoregrapher() {
+		return contactNumberOfTheChoregrapher;
+	}
+
+	public void setContactNumberOfTheChoregrapher(long contactNumberOfTheChoregrapher) {
+		this.contactNumberOfTheChoregrapher = contactNumberOfTheChoregrapher;
+	}
+	
+	
+
+}
