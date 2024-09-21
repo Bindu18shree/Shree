bd1393a3b819cb67262fd350c21620d5d91ff335
unknown <bindushreep.xworkz@gmail.com>
2024-09-21 12:09:22 +0530

    Main method declaration

---
+++ Main method declaration/Bindu.java
@@ -0,0 +1,6 @@
+class Bindu{
+public static void main(String[] args){
+
+System.out.println("hi");
+}
+}
---
+++ Main method declaration/Sambharmanasa.java
@@ -0,0 +1,5 @@
+class Sambharmanasa{
+	public static void main(String[] args){
+		System.out.println("Sambhar prepared by manasa was delicious ");
+}
+}
