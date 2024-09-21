fb71cb23aa567e8b7c462d20bc2ee3f9441d3b76
unknown <bindushreep.xworkz@gmail.com>
2024-09-21 12:36:05 +0530

    Setters and getters of instance and static variables

---
+++ setter and getter of instance and static variables/FilmIndustry.java
@@ -0,0 +1,89 @@
+class FilmIndustry{
+	
+	static String nameOfTheFilmIndustry;
+	
+	static String nameOfThePopularActorInIndustry;
+	
+	static Byte ageOfTheActor;
+	
+	static Long contactNumberOfHisManager;
+	
+	static Integer noOfMoviesActed;
+	
+	static Short noOfUpcomingMovies;
+	
+	static Double approxPaymentTakenPerMovie;
+	
+	static Boolean isActorAliveOrNot;
+	
+	static Float noOfYearsPassedSinceHeLeft;
+	
+	
+	public static void setNameOfTheFilmIndustry(String nameOfTheFilmIndustry){
+		FilmIndustry.nameOfTheFilmIndustry = nameOfTheFilmIndustry;
+	}
+	
+	public static void setNameOfThePopularActorInIndustry(String nameOfThePopularActorInIndustry){
+		FilmIndustry.nameOfThePopularActorInIndustry = nameOfThePopularActorInIndustry;
+	}
+	
+	public static void setAgeOfTheActor(Byte ageOfTheActor){
+		FilmIndustry.ageOfTheActor = ageOfTheActor;
+	}
+	
+	public static void setContactNumberOfHisManager(Long contactNumberOfHisManager){
+		FilmIndustry.contactNumberOfHisManager = contactNumberOfHisManager;
+	}
+	
+	public static void setNoOfMoviesActed(Integer noOfMoviesActed){
+		FilmIndustry.noOfMoviesActed = noOfMoviesActed;
+	}
+	
+	public static void setNoOfUpcomingMovies(Short noOfUpcomingMovies){
+		FilmIndustry.noOfUpcomingMovies = noOfUpcomingMovies;
+	}
+	
+	public static void setApproxPaymentTakenPerMovie(Double ApproxPaymentTakenPerMovie){
+		FilmIndustry.approxPaymentTakenPerMovie = approxPaymentTakenPerMovie;
+	}
+	
+	public static void setIsActorAliveOrNot(Boolean isActorAliveOrNot){
+		FilmIndustry.isActorAliveOrNot = isActorAliveOrNot;
+	}
+	
+	public static void setNoOfYearsPassedSinceHeLeft(Float noOfYearsPassedSinceHeLeft){
+		FilmIndustry.noOfYearsPassedSinceHeLeft = noOfYearsPassedSinceHeLeft;
+	}
+	
+	
+	public static String getNameOfTheFilmIndustry(){
+		return nameOfTheFilmIndustry;
+	}
+	
+	public static String getNameOfThePopularActorInIndustry(){
+		return nameOfThePopularActorInIndustry;
+	}
+	
+	public static Long getContactNumberOfHisManager(){
+		return contactNumberOfHisManager;
+	}
+	
+	public static Integer getNoOfMoviesActed(){
+		return noOfMoviesActed;
+	}
+	
+	public static Double getApproxPaymentTakenPerMovie(){
+		return approxPaymentTakenPerMovie;
+	}
+	
+	public static Boolean isActorAliveOrNot(){
+		return isActorAliveOrNot;
+	}
+	
+	public static Float getNoOfYearsPassedSinceHeLeft(){
+		return noOfYearsPassedSinceHeLeft;
+	}
+	
+	
+	
+}
