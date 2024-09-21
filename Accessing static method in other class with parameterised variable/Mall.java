afad580059f22d58833cc1d3b3df722631565bcf
unknown <bindushreep.xworkz@gmail.com>
2024-09-21 11:46:58 +0530

    Accessing static method in runner class with parameterized variable

---
+++ Accessing static method in other class with parameterised variable/Mall.java
@@ -0,0 +1,22 @@
+class Mall{
+	
+	public static void pheonixMallOfAsia(String location){
+		
+		System.out.println("The Pheonix Mall Of Asia is located in : "+location);
+	}
+	
+	public static void sportsWear(String brand){
+		
+		System.out.println("The Sports Wear brand in the Mall :"+brand);
+	}
+	
+	public static void electronicsAndGadgets(String brand){
+		
+		System.out.println("The Electronics and Gadgets Store :"+brand);
+	}
+	
+	public static void fashionAccessories(String brand){
+		
+		System.out.println("The Fashion Accessories Shop :"+brand);
+	}
+}
---
+++ Accessing static method in other class with parameterised variable/SeaFood.java
@@ -0,0 +1,10 @@
+class SeaFood{
+	
+	public static void crabRoast(float cost){
+		System.out.println("The Cost of the Crab Roast =="+cost);
+	}
+	
+	public static void lemonPrawns(int cost){
+		System.out.println("The cost of the Lemon Prawns =="+cost);
+	}
+}
