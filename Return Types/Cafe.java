0d1ece2ec2c84d5a34715cd469b940f8749d8416
unknown <bindushreep.xworkz@gmail.com>
2024-09-21 12:10:45 +0530

    Return Type

---
+++ Return Types/Cafe.java
@@ -0,0 +1,12 @@
+class Cafe{
+	
+	public static String name(String CafeName){
+		
+		return "Rameshwaram Cafe";
+	}
+	
+	public static int Branches(String branch1, String branch2){
+		
+		return 4;
+	}
+}
